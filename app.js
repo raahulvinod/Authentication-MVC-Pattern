41b8b9fb0bd90921f995e6b2695deabc7f3e1de6
Rahul vinod <rahulvinod135@gmail.com>
2023-03-28 12:52:31 +0530

    spliting routes, extracting config setting and custom middleware, creating first model and adding update & dlete & fetch

---
+++ app.js
@@ -0,0 +1,38 @@
+const path = require('path');
+
+const express = require('express');
+const session = require('express-session');
+const sessionConfig = require('./config/session');
+const csrf = require('csurf');
+
+const db = require('./data/database');
+const authRoutes = require('./routes/auth');
+const blogRoutes = require('./routes/blog');
+const authMiddleware = require('./middlewares/auth-middlewares');
+
+const mongodbSessionStore = sessionConfig.createSessionStore(session);
+
+const app = express();
+
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
+
+app.use(session(sessionConfig.createSessionConfig(mongodbSessionStore)));
+app.use(csrf());
+
+app.use(authMiddleware); 
+
+app.use(blogRoutes);
+app.use(authRoutes);
+
+app.use(function(error, req, res, next) {
+  res.render('500');
+})
+
+db.connectToDatabase().then(function () {
+  app.listen(3000);
+});
